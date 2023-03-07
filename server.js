c3f2717e6af37a25ab3afba4149dd8fc2e86db19
Samrat Kafle <skafle3@leomail.tamuc.edu>
2023-03-06 20:42:58 -0600

    Updated For Production

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/3-6-2023'));
+app.get('/*', function(req,res)
+
+{res.sendFile(path.join(__dirname+'/dist/3-6-2023/index.html'));});
+
+app.listen(process.env.PORT || 8080);
